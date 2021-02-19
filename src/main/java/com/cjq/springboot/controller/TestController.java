27f8c55e5b54d53ce41cb70f3dca6c3b22d2bfad
chenjunqi <chenjunqi@thunisoft.com>
2021-02-19 14:07:03 +0800

    增加代码

--- src/main/java/com/cjq/springboot/controller/TestController.java
+++ src/main/java/com/cjq/springboot/controller/TestController.java
@@ -6,6 +6,8 @@
  */
 package com.cjq.springboot.controller;
 
+import com.cjq.studyClientStarter.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private HelloService helloService;
+
     @RequestMapping("/")
     public String home(){
-        return "hello world";
+        return helloService.sayHello();
     }
 }
